6dc0239be8ecaaeefebc5a9f0d1d0796f67c847c
Elrey Belmonti and Kelsey Langlois <elrey@>
2017-09-20 09:20:59 -0700

    Add Patron class with basic functionality.

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,4 +1,46 @@
 #!/usr/bin/env ruby
 
 class Patron
+  attr_reader :id, :first_name, :last_name
+
+  def initialize(attributes)
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+    @id = attributes[:id] || nil
+  end
+
+  def full_name
+    @first_name + " " + @last_name
+  end
+
+  def save
+    results = DB.exec("INSERT INTO patrons (first_name, last_name) VALUES ('#{@first_name}', '#{@last_name}') RETURNING id;")
+    @id = results.first["id"].to_i
+  end
+
+  def ==(other_patron)
+    (full_name == other_patron.full_name) & (@id == other_patron.id)
+  end
+
+  def self.all
+    results = DB.exec("SELECT * FROM patrons;")
+    results.map do |result|
+      Patron.new({
+        :first_name => result["first_name"],
+        :last_name => result["last_name"],
+        :id => result["id"].to_i
+      })
+    end
+  end
+
+  def self.find(search_term)
+    results = DB.exec("SELECT * FROM patrons WHERE first_name = '#{search_term}' OR last_name = '#{search_term}' OR id = #{search_term};")
+    results.map do |result|
+      Patron.new({
+        :first_name => result["first_name"],
+        :last_name => result["last_name"],
+        :id => result["id"].to_i
+      })
+    end
+  end
 end
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -3,4 +3,46 @@
 require 'spec_helper'
 
 describe('Patron') do
+  let (:patron) { Patron.new({:first_name => "Bob", :last_name => "Smith"}) }
+
+  it "stores a patron's first & last name and id, initializing id to nil" do
+    expect(patron.id).to eq(nil)
+    expect(patron.first_name).to eq("Bob")
+    expect(patron.last_name).to eq("Smith")
+  end
+
+  describe('#full_name') do
+    it "returns a patron's first and last names together" do
+      expect(patron.full_name).to eq("Bob Smith")
+    end
+  end
+
+  describe('#==') do
+    it "is equal when two patrons' first & last names and ids are the same" do
+      patron2 = Patron.new({:first_name => "Bob", :last_name => "Smith"})
+      expect(patron).to eq(patron2)
+    end
+  end
+
+  describe('#save') do
+    it "saves a patron to the database" do
+      patron.save
+      expect(Patron.all).to eq([patron])
+    end
+  end
+
+  describe('.all') do
+    it "returns all patrons saved to the database" do
+      expect(Patron.all).to eq([])
+    end
+  end
+
+  describe('.find') do
+    it "returns all patrons whose first or last name or id matches the search term" do
+      patron.save
+      patron2 = Patron.new({:first_name => "Bob", :last_name => "Smith"})
+      patron2.save
+      expect(Patron.find(patron.id)).to eq([patron])
+    end
+  end
 end
